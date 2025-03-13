4d7300c066876cfcc2f4d69a222b0043a59d592e
mateus <matheus2022y@gmail.com>
2025-03-13 16:54:26 -0300

    primeiro commit

---
+++ src/page/Home/index.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import { FaTrash } from "react-icons/fa";
+import Api from "../../service/Api";
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function getUsers() {
+    const usersFromApi = await Api.get('/usuarios')
+
+    setUsers(usersFromApi.data)
+    console.log(users)
+  }
+
+  async function creatUsers() {
+     try {
+      const value =
+      await Api.post('/usuarios',{
+        name: inputName.current.value,
+        age: inputAge.current.value,
+        email: inputEmail.current.value,
+       })
+       console.log(value)
+     } catch (error) {
+      console.log(error)
+     }
+   
+     
+    console.log(inputName)
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, []);
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input type="text" name="nome" placeholder="Nome" ref={inputName} />
+        <input type="number" name="idade" placeholder="Idade" ref={inputAge} />
+        <input type="email" name="email" placeholder="E-mail" ref={inputEmail} />
+        <button type="submit" onClick={creatUsers}>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.Id} className="infor">
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>
+            </p>
+            <p>
+              E-mail: <span>{user.email}</span>
+            </p>
+          </div>
+          <button className="icon-btn" >
+            <FaTrash size={24} color="black" />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
